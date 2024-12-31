01209c13a772ef4d48c5a5dcd4946d12ac59b1d7
Salijoghli <guga.kakhadze12@gmail.com>
2024-12-31 19:14:41 +0400

    created messages and conversation. added timestamps to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullname: {
-    required: true,
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      required: true,
+      type: String,
+    },
+    username: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    password: {
+      required: true,
+      type: String,
+      minLength: 8,
+    },
+    gender: {
+      required: true,
+      type: String,
+      enum: ["male", "female"],
+      default: "male",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    required: true,
-    type: String,
-    unique: true,
-  },
-  password: {
-    required: true,
-    type: String,
-    minLength: 8,
-  },
-  gender: {
-    required: true,
-    type: String,
-    enum: ["male", "female"],
-    default: "male",
-  },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
